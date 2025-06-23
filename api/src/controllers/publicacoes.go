09d081ddcc3bafabe3295cd36c915ceb318c8b0a
André Teixeira <andre-x8@hotmail.com>
2025-06-23 20:58:51 -0300

    adiciona modulo de publicacao

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func CriaPublicacao(w http.ResponseWriter, r *http.Request) {
+	// Implementação da função para criar uma publicação
+	usuarioID, erro := autenticacao.ExtrairUsuarioId(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorID = usuarioID
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao.ID, erro = repositorio.Criar(publicacao)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
+
+}
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+func BuscarPublicacoesPorId(w http.ResponseWriter, r *http.Request) {}
+
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+	// Implementação da função para atualizar uma publicação
+}
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {}
--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -17,6 +17,7 @@ type Rota struct {
 func Configurar(r *mux.Router) *mux.Router {
 	rotas := rotasUsuarios //Essa variavel faz a ligação com o usuarios.go
 	rotas = append(rotas, rotaLogin)
+	rotas = append(rotas, rotasPublicacoes...)
 	for _, rota := range rotas {
 
 		if rota.RequerAutenticacao {
