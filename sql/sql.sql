032e684acdcea7ff43245a26c4d37ff536204830
André Teixeira <andre-x8@hotmail.com>
2025-06-23 21:06:54 -0300

    adiciona arquivo sql

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,6 +1,9 @@
 CREATE DATABASE IF NOT EXISTS devbook DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE devbook;
 
+
+DROP TABLE IF EXISTS publicacoes;
+DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuario;
 
 CREATE TABLE usuario (
@@ -26,3 +29,15 @@ CREATE TABLE seguidores(
 
     PRIMARY KEY(usuario_id,seguidor_id)
 )
+
+CREATE TABLE publicacoes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(50) NOT NULL,
+    conteudo VARCHAR(300) NOT NULL,
+    autor_id INT NOT NULL,
+    FOREIGN KEY (autor_id)
+    REFERENCES usuario(id)
+    ON DELETE CASCADE,
+    curtidas INT DEFAULT 0,
+    criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
