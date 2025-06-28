546c16312f5392e949b9cb7bb6538a609789c4ae
André Teixeira <andre-x8@hotmail.com>
2025-06-28 11:13:44 -0300

    adiciona liberacao de cors

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -19,15 +19,17 @@ func Configurar(r *mux.Router) *mux.Router {
 	rotas = append(rotas, rotaLogin)
 	rotas = append(rotas, rotasPublicacoes...)
 	for _, rota := range rotas {
-
 		if rota.RequerAutenticacao {
-			r.HandleFunc(rota.URI,
-				middlewares.Logger(middlewares.Autenticar(rota.Funcao)),
-			).Methods(rota.Metodo)
+			r.HandleFunc(
+				rota.URI,
+				middlewares.Cors(middlewares.Logger(middlewares.Autenticar(rota.Funcao))),
+			).Methods(rota.Metodo, "OPTIONS")
 		} else {
-			r.HandleFunc(rota.URI, middlewares.Logger(rota.Funcao)).Methods(rota.Metodo)
+			r.HandleFunc(
+				rota.URI,
+				middlewares.Cors(middlewares.Logger(rota.Funcao)),
+			).Methods(rota.Metodo)
 		}
-
 	}
 
 	return r
